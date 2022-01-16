d551a49d6f21fc8cce9a2cfebf3b6e3abe9069fa
Valentin <valentinvalentinov027@gmail.com>
2022-01-16 20:38:59 +0200

    Added Form Functionality - Created the Form and Map Requests

--- BasicWebServer/BasicWebServer.Demo/Startup.cs
+++ BasicWebServer/BasicWebServer.Demo/Startup.cs
@@ -6,10 +6,17 @@
 
     public class Startup
     {
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+   Name: <input type='text' name='Name'/>
+   Age: <input type='number' name ='Age'/>
+<input type='submit' value ='Save' />
+</form>";
+
         public static void Main()
             => new HttpServer(routes => routes
                 .MapGet("/", new TextResponse("Hello from the server!"))
-                .MapGet("/HTML", new HtmlResponse("<h1>HTML response</h1>"))
+                .MapGet("/HTML", new HtmlResponse(HtmlForm))
+                .MapPost("/HTML", new TextResponse(""))
                 .MapGet("/Redirect", new RedirectResponse("https://softuni.org/")))
             .Start();
     }
